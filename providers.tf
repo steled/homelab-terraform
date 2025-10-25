a80693e2b939ebf8aeaeea59f07af35ca275995d
steled <steled@gmx.net>
2025-10-25 05:40:38 +0200

    [tf] provider update

--- providers.tf
+++ providers.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0.1"
+      version = "~> 3.0.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.35"
+      version = "~> 2.38.0"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = "~> 2.1"
+      version = "~> 2.1.3"
     }
   }
 }
