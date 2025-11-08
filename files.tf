2477e26fef515b2564332013e93485c913f5cc17
steled <steled@gmx.net>
2025-11-08 07:01:05 +0100

    [tf] cleanup

--- files.tf
+++ files.tf
@@ -1,33 +1,3 @@
-# resource "ssh_resource" "files" {
-#   host     = var.server.host
-#   user     = var.server.user
-#   # agent    = true
-
-#   file {
-#     source      = "files/crontab"
-#     destination = "/tmp/steled-crontab"
-#     permissions = "0755"
-#     owner       = "steled"
-#     group       = "steled"
-#   }
-
-#   # file {
-#   #   source      = "files/preparation.sh"
-#   #   destination = "/tmp/preparation.sh"
-#   #   permissions = "0755"
-#   #   owner       = "steled"
-#   #   group       = "steled"
-#   # }
-
-#   commands = [ 
-#     "crontab /tmp/steled-crontab",
-#     # "/tmp/preparation.sh"
-#   ]
-
-#   private_key = file(var.server.private_key)
-#   # timeout = "10m"
-# }
-
 locals {
   file_hashes = {
     for f in fileset("${path.module}/files", "**") :
